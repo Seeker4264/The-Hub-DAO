644a5b4c842d911cd1e24d792527885e2954f71b
Sebas4624 <h.sebastian@correo.unimet.edu.ve>
2024-07-19 12:23:39 -0400

    find communities by tag section

--- src/qvote_project_frontend/src/components/NavBar.jsx
+++ src/qvote_project_frontend/src/components/NavBar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 function NavBar() {
 
-
     return (
         <div className={styles.topnav}>
             <div className={styles.logo}>Logo</div>
-            <div className={styles.left}>Home</div>
-            <div className={styles.left}>Communities</div>
+            <NavLink to='/' className={({ isActive, isPending, isTransitioning }) =>
+                [
+                    isPending ? "pending" : "",
+                    isActive ? styles.active : "",
+                    isTransitioning ? "transitioning" : "",
+                ].join(" ")
+            }>
+                <div className={styles.left}>Home</div>
+            </NavLink>
+
+            <NavLink to='/communities' className={({ isActive, isPending, isTransitioning }) =>
+                [
+                    isPending ? "pending" : "",
+                    isActive ? styles.active : "",
+                    isTransitioning ? "transitioning" : "",
+                ].join(" ")
+            }>
+                <div className={styles.left}>Communities</div>
+            </NavLink>
             <div className={styles.searchbar}><input type="text" placeholder='Search Hub' /></div>
             <div className={styles.right}>Register</div>
         </div>
--- src/qvote_project_frontend/src/pages/Communities.jsx
+++ src/qvote_project_frontend/src/pages/Communities.jsx
@@ -5,7 +5,14 @@ import styles from './Communities.module.css';
 function Communities() {
     return (
         <div className={styles.container}>
-            Popular
+            <div className={styles.communityContainer}>
+                <div className={styles.containerHeader}>
+                    Search by tags
+                </div>
+                <div className={styles.containerContent}>
+                    Wawa
+                </div>
+            </div>
         </div>
     )
 };
